b5e14a22feaa85ded4ec3c3926980dfca6232b75
Jeffrey Nelson <jt_nelson@rogers.com>
2016-10-20 21:16:58 -0400

    Phase 1 completed (ish)
    Lines can now be drawn in both directions
    Addes filters for the canvases (Border fade)
    Re-structured particles to make it function better
    Currently testing 24 particles running smoothly

--- sub_projects/visualizer/visualizer.js
+++ sub_projects/visualizer/visualizer.js
@@ -12,6 +12,7 @@ class Visualizer{
 		this.canvas.id = "canvas_visualizer";
 		this.canvas.width = this.canvas_buffer.width = this.canvas_mask.width = width;
 		this.canvas.height = this.canvas_buffer.height = this.canvas_mask.height = height;
+		//this.canvas.style.webkitFilter = "blur(1px)";
 		
 		this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
 		this.ctx_buffer.fillRect(0,0,this.canvas_buffer.width,this.canvas_buffer.height);
@@ -41,6 +42,7 @@ class Visualizer{
 	draw(){
 		
 		this.system.draw(this.ctx_mask);
+		Filter.faded_border(this.canvas_mask, 150);
 		this.ctx.drawImage(this.canvas_mask,0,0,this.canvas.width,this.canvas.height);
 		
 		
@@ -66,12 +68,13 @@ class ParticleSystem{
 		this.y = 250;
 		this.radius = 3;
 		this.particles = [];
-		this.particle_count = 4;
+		this.particle_count = 24;
 	}
 	
 	init(){
 		for(var i = 0; i < this.particle_count; i++){
 			this.particles[i] = new Particle();
+			this.particles[i].init();
 		}
 	}
 	
@@ -90,26 +93,29 @@ class ParticleSystem{
 
 class Particle{
 	constructor(){
-		this.radius = 4;
-		this.speed = 1;
-		this.change_timer = 0;
-		this.direction = 0;
-		this.update_change_timer_max(this.direction);
-
-		
+		//Magical attributes
+		this.magic_direction_constant = 0.707;
+		this.magic_opacity_constant = 0.07142;
 		
 		//Physical attributes
+		this.radius = (Math.random() * 12)|0 + 2;
+		this.speed = this.radius / 3;
 		this.length = 600 + (Math.random() * 150 * this.radius)|0;
+		this.trail_width = Math.ceil(this.radius / 3);
 		
-		//Virtual Attributes
-		this.position = new Vector((Math.random() * 1000)|0,(Math.random() * 400)|0);
-		this.velocity = [];
-		this.velocity[-1] = new Vector(this.speed / 1.414,-this.speed / 1.414);
-		this.velocity[0] = new Vector(this.speed,0);
-		this.velocity[1] = new Vector(this.speed / 1.414,this.speed / 1.414);
-		
+		//Position Attributes
+		this.direction = new Vector(this.get_direction(),0); //x = -1 (Left) x = 1 (Right), y = -1 (Up) y = 0 (Straight) y = 1 (Down) 
+		this.position = new Vector((Math.random() * window.innerWidth)|0,(Math.random() * window.innerHeight)|0);
+		this.velocity = new Vector(this.speed * this.direction.x,0);
+
+		//Direction change timer variables
+		this.dct = 0;
+		this.dctm = this.get_new_dctm();
+
+		//Vertex array for drawing the particle trail
 		this.vertices = [];
-		this.vertices[0] = new Vector(this.position.x - this.length, this.position.y);
+		//Initial trail end
+		this.vertices[0] = new Vector(this.position.x - this.length * this.direction.x , this.position.y);
 	}
 	
 	init(){
@@ -118,38 +124,65 @@ class Particle{
 	
 	update(){
 		
-		this.position.add(this.velocity[this.direction]);
-		
-		this.change_timer++;
-		//Makes diagonals shorter
-		
-		if (this.change_timer > this.change_timer_max - this.direction_mod|0){
-			this.change_timer = 0;
-			this.change_direction();
-			this.update_change_timer_max(this.direction);
+		//Update Velocity
+		if(0 == this.direction.y){
+			this.velocity.set(this.speed * this.direction.x,0);
+		}else{
+			this.velocity.set(this.speed * this.direction.x  * this.magic_direction_constant, this.speed * this.direction.y  * this.magic_direction_constant);
 		}
+		
+		//Update Position
+		this.position.add(this.velocity);
+		
+		//Change direction if direction change timer is greater than direction change timer max
+		if (++this.dct > this.dctm){
+			this.dct = 0;
+			this.change_direction();
+		}
+		
 		//Check if the first vertex is unused
 		if("undefined" != typeof this.vertices[1]){
-			//TO DO: multi-directional
-			if(this.vertices[1].x < this.position.x - this.length){
-				this.shift_vertices();
+			if(-1 == this.direction.x){
+				if(this.vertices[1].x > this.position.x + this.length)
+					this.shift_vertices();
+			}else{
+				if(this.vertices[1].x < this.position.x - this.length)
+					this.shift_vertices();
 			}
+
+		}
+		
+		//Off the right side
+		if(this.vertices[0].x > screen.width && 1 == this.direction.x){
+			this.position = new Vector((Math.random() * -100)|0,(Math.random() * window.innerHeight)|0);
+			this.vertices = [];
+			this.vertices[0] = new Vector(this.position.x - this.length, this.position.y);
+			this.direction.y = 0;
+		}
+		
+		//Off the left side
+		if(this.vertices[0].x < 0 && -1 == this.direction.x){
+			this.position = new Vector((Math.random() * 100)|0 + window.innerWidth,(Math.random() * window.innerHeight)|0);
+			this.vertices = [];
+			this.vertices[0] = new Vector(this.position.x + this.length, this.position.y);
+			this.direction.y = 0;
 		}
 	}
 	
 	draw(ctx){
-		ctx.fillStyle="#fff";
-		ctx.beginPath();
-		ctx.arc(this.position.x|0,this.position.y|0,this.radius,0,2*Math.PI);
-		ctx.fill();
 		
-		ctx.lineWidth=Math.ceil(this.radius / 3);
 		
-		var my_gradient=ctx.createLinearGradient(this.position.x - this.length,0,this.position.x,0);
+		
+		//Set trail width
+		ctx.lineWidth=this.trail_width;
+		
+		//Prepare the gradient to make the trail fade out
+		var my_gradient=ctx.createLinearGradient(this.position.x - this.length * this.direction.x,0,this.position.x,0);
 		my_gradient.addColorStop(0,"rgba(255,255,255,0)");
-		my_gradient.addColorStop(1,"rgba(255,255,255,1)");
+		my_gradient.addColorStop(1,"rgba(255,255,255," + this.radius * this.magic_opacity_constant + ")");
 		ctx.strokeStyle = my_gradient;
-		//ctx.fillRect(this.x - 800, this.y - 1, 800, 2);
+
+		//Draw trail
 		ctx.beginPath();
 		ctx.moveTo(this.vertices[0].x,this.vertices[0].y);
 		for(var i = 1; i < this.vertices.length; i++){
@@ -157,6 +190,14 @@ class Particle{
 		}
 		ctx.lineTo(this.position.x,this.position.y);
 		ctx.stroke();
+		
+		
+		//Draw circle
+		var c =((255 * this.radius * this.magic_opacity_constant)|0);
+		ctx.fillStyle="rgba("+ c +","+ c +","+ c +",1)";
+		ctx.beginPath();
+		ctx.arc(this.position.x|0,this.position.y|0,this.radius,0,2*Math.PI);
+		ctx.fill();
 	}
 	
 	//Shifts vertices to remove the oldest element
@@ -171,26 +212,42 @@ class Particle{
 
 	}
 	
+	//Handles all functions relating to changing the direction of the particle
 	change_direction(){
+		
+		//Set new vertex
 		this.vertices[this.vertices.length] = new Vector(this.position.x, this.position.y);
 		
-		switch(this.direction){
-			case 0:
-				this.direction = Math.floor(Math.random() * 2) == 0 ? 1 : -1;
-				break;
-			case -1:
-			case 1:
-				this.direction = 0;
-				break;
-			
-		}
+		//Set new direction change timer maximum
+		this.dctm = this.get_new_dctm();
 		
+		//Change vertical direction
+		if(0 == this.direction.y){
+			
+			//Up or Down
+			this.direction.y = this.get_direction();
+			
+			//Scale the time down a bit for diagonals
+			this.dctm *= 0.7 + (Math.random() * 0.25);
+			
+		}else{
+			//Middle
+			this.direction.y = 0;
+		}
+
 	}
 	
-	update_change_timer_max(direction){
-		this.change_timer_max = 160 + (Math.random() * 300)|0;
-		this.direction_mod = this.change_timer_max * 0.6 + (Math.random() * 0.35) * Math.abs(direction);
+	//Gets a new direction change timer maximum value
+	get_new_dctm(){
+		return 160 + (Math.random() * 300)|0;
 	}
+	
+	//Returns -1 or 1
+	get_direction(){
+		return ((Math.random() * 2)|0) == 0 ? -1 : 1;
+	}
+	
+	
 }
 
 class Vector{
@@ -204,22 +261,43 @@ class Vector{
 		this.x += vector.x;
 		this.y += vector.y;
 	}
+	
+	set(x, y){
+		this.x = x;
+		this.y = y;
+	}
 }
 
-/*
-function reduce_whites(ctx, speed){
-	try{
-		var imgd = ctx.getImageData(0,0,500,500);
-		var pix = imgd.data;
+class Filter{
+	static faded_border(canvas,border_size){
+		var ctx = canvas.getContext("2d");
 		
-		for (var i = 0, n = pix.length; i < n; i += 4){
-			pix[i] = pix[i] - speed < 0 ? 0 : pix[i] - speed;
-			pix[i+1] = pix[i+1] - speed < 0 ? 0 : pix[i+1] - speed;
-			pix[i+2] = pix[i+2] - speed < 0 ? 0 : pix[i+2] - speed;	
-		}
-
-		ctx.putImageData(imgd, 0, 0);
-	}catch(e){
-		console.log(e);
+		//Left border
+		var my_gradient=ctx.createLinearGradient(0,0,border_size,0);
+		my_gradient.addColorStop(0,"rgba(0,0,0,1)");
+		my_gradient.addColorStop(1,"rgba(0,0,0,0)");
+		ctx.fillStyle = my_gradient;
+		ctx.fillRect(0,0,border_size,canvas.height);
+		
+		//Top border
+		my_gradient=ctx.createLinearGradient(0,0,0,border_size);
+		my_gradient.addColorStop(0,"rgba(0,0,0,1)");
+		my_gradient.addColorStop(1,"rgba(0,0,0,0)");
+		ctx.fillStyle = my_gradient;
+		ctx.fillRect(0,0,canvas.width,border_size);
+		
+		//Right border
+		my_gradient=ctx.createLinearGradient(canvas.width,0,canvas.width - border_size,0);
+		my_gradient.addColorStop(0,"rgba(0,0,0,1)");
+		my_gradient.addColorStop(1,"rgba(0,0,0,0)");
+		ctx.fillStyle = my_gradient;
+		ctx.fillRect(canvas.width - border_size,0,canvas.width,canvas.height);
+		
+		//Bottom border
+		my_gradient=ctx.createLinearGradient(0,canvas.height,0,canvas.height - border_size);
+		my_gradient.addColorStop(0,"rgba(0,0,0,1)");
+		my_gradient.addColorStop(1,"rgba(0,0,0,0)");
+		ctx.fillStyle = my_gradient;
+		ctx.fillRect(0,canvas.height - border_size,canvas.width,canvas.height);
 	}
-}*/
+}
